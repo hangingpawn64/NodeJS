aa5b1fb37b7367d75e5bdaced0609b705f103e86
AkshitTupkar <2024bcs156@sggs.ac.in>
2025-08-03 21:26:03 +0530

    explored modules

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log("hello world!")
+// const {add, sub} = require("./math");
+
+// console.log("the sum is " + add(2,4) + " the sub is "+  sub(2,4));
+
+const math = require("fs");
+
+console.log("value of math is", math )
