6d9d7ff9eee57bd4181ac710132c2b0e7197eb7f
AkshitTupkar <2024bcs156@sggs.ac.in>
2025-08-11 22:07:33 +0530

    url management

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 const myServer = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req. Recieved\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log, (err, data) =>{
-        switch(req.url){
+        switch(myUrl.pathname){
             // case `${data}`: res.end("data");
             // break;
             case '/': res.end("HomePage");
             break;
-            case '/about': res.end("Hello This is Akshit");
+            case '/about': 
+            const username=myUrl.query.myname; res.end(`hi ${username}`);
             break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end("here are your result for "+ search ); 
+
             default: res.end("yoyoyo either / or about");
         }
     // res.end("hello from server again!");
